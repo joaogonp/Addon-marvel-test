c7750b1a3cdf36aaa658390e35ff5a02d466e480
João <jpnapsp@gmail.com>
2025-05-06 16:47:37 +0100

    Update xmenData.js

--- Data/xmenData.js
+++ Data/xmenData.js
@@ -1,2 +1,10 @@
 module.exports = [
-]
+  {
+    "title": "Spidey Super Stories",
+    "type": "series",
+    "imdbId": "tt27826524",
+    "id": "marvel_tt27826524",
+    "releaseYear": "1974",
+    "poster": "https://image.tmdb.org/t/p/original/cSHETWKC1JPQ3cNmpMCx9vMPoBs.jpg"
+  },
+];
