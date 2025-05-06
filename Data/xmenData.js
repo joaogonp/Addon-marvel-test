e0856b67a7f9b3ddd9d249ac3e73aaa934194ec3
João <jpnapsp@gmail.com>
2025-05-06 16:54:58 +0100

    Update xmenData.js

--- Data/xmenData.js
+++ Data/xmenData.js
@@ -1,1 +1,27 @@
+module.exports = [
+  {
+    "title": "Spidey Super Stories",
+    "type": "series",
+    "imdbId": "tt27826524",
+    "id": "marvel_tt27826524",
+    "releaseYear": "1974",
+    "poster": "https://image.tmdb.org/t/p/original/cSHETWKC1JPQ3cNmpMCx9vMPoBs.jpg"
+  },
+  {
+    "title": "The Amazing Spider-Man",
+    "type": "series",
+    "imdbId": "tt0076975",
+    "id": "marvel_tt0076975",
+    "releaseYear": "1977",
+    "poster": "https://image.tmdb.org/t/p/original/hJUka4mFGp9L3FNw9uJiUg6mkYG.jpg"
+  },
+  {
+    "title": "The Incredible Hulk",
+    "type": "series",
+    "imdbId": "tt0077031",
+    "id": "marvel_tt0077031",
+    "releaseYear": "1977",
+    "poster": "https://image.tmdb.org/t/p/original/8q7uFuNuMWTWfcpnCpBuZa3KZXx.jpg"
+  }
+];
 
