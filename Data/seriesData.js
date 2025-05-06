84137e293d276cdced2b50534b4a0e7629f20d0f
João <jpnapsp@gmail.com>
2025-05-06 16:38:10 +0100

    Update seriesData.js

--- Data/seriesData.js
+++ Data/seriesData.js
@@ -2,8 +2,8 @@ module.exports = [
   {
     "title": "Spidey Super Stories",
     "type": "series",
-    "imdbId": "tt27998599",
-    "id": "marvel_tt27998599",
+    "imdbId": "tt27826524",
+    "id": "marvel_tt27826524",
     "releaseYear": "1974",
     "poster": "https://image.tmdb.org/t/p/original/cSHETWKC1JPQ3cNmpMCx9vMPoBs.jpg"
   },
